f51bdd75f93864ce23f27b5e0afd4b47ea98913d
AndresOberhelman <andres.w.oberhelman@gmail.com>
2022-05-16 12:50:26 -0500

    Added pages and created requests

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Routes, Route, Link } from "react-router-dom";
 import Navbar from './components/navbar.component';
- import ExerciseList from "./components/exercises-list.component";
- import EditExercise from "./components/edit-exercises.component";
- import CreateUser from "./components/create-user.component";
- import CreateExercise from "./components/create-exercise.component";
+import ExerciseList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateUser from "./components/create-user.component";
+import CreateExercise from "./components/create-exercise.component";
 
 
 function App() {
   return(
- <Navbar>
-  <Routes>
-    <Route path="/" element={ExerciseList} />
-    <Route path="/edit/:id" element= {EditExercise} />
-    <Route path="/create" element= {CreateExercise} />
-    <Route path="/user" element = {CreateUser} />
-  </Routes>
-  </Navbar>
+    <><Navbar />
+    <Routes>
+      <Route path="/" element={<ExerciseList />} />
+      <Route path="/edit/:id" element={<EditExercise />} />
+      <Route path="/create" element={<CreateExercise />} />
+      <Route path="/user" element={<CreateUser />} />
+    </Routes></>
+  
   )
 }
 
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component { //how components start
   render() { // all components must render something
     return ( // bootstrap navbar converted to work for app
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <Link to="/" className="navbar-brand">Exercise-Tracker</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
