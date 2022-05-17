727afe1f00ccab1919a645e91d585af03eb1d495
AndresOberhelman <andres.w.oberhelman@gmail.com>
2022-05-17 00:59:59 -0500

    Changes

--- backend/server.js
+++ backend/server.js
@@ -37,3 +37,7 @@ app.use(express.static(path.join(__dirname, '../src/build')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../src/build/index.html'))
 })
+
+if(process.env.NODE_ENV == 'production'){
+  app.use(express.static('./build'));
+}
