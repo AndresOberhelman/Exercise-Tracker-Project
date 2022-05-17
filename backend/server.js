cc8d43e8ec6fdde5d27bd895a1e770ac9a935bd7
AndresOberhelman <andres.w.oberhelman@gmail.com>
2022-05-17 01:18:39 -0500

    Changes

--- backend/server.js
+++ backend/server.js
@@ -31,13 +31,10 @@ app.listen(port, () => {
     console.log(`Server is running on port: ${port}`); //start the server
 });
 
-app.use(express.static(path.join(__dirname, '../src/build')))
+app.use(express.static(path.join(__dirname, '../build')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../src/build/index.html'))
+  res.sendFile(path.join(__dirname + '/../build/index.html'))
 })
 
-if(process.env.NODE_ENV == 'production'){
-  app.use(express.static('./build'));
-}
--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-
+const url = `${process.env.API_URL}`
 export default class CreateExercise extends Component {
   constructor(props) {
     super(props);
